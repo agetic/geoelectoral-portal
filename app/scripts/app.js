e7fa9131b5e86c252e4c2396380cd16edd0e32c6
Elmer Mendoza <emendoza@geo.gob.bo>
2014-08-20 18:37:32 -0400

    Yoeman con Angular.js

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name geoelectoralFrontendApp
+ * @description
+ * # geoelectoralFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('geoelectoralFrontendApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geoelectoralFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the geoelectoralFrontendApp
+ */
+angular.module('geoelectoralFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
