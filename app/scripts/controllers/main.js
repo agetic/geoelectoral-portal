68529fb5f05cd75645f443f187676afae53b4f22
Elmer Mendoza <emendoza@geo.gob.bo>
2014-08-29 11:45:58 -0400

    Tabla - Porcentaje de votos en las elecciones generales

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,37 @@
  * Controller of the geoelectoralFrontendApp
  */
 angular.module('geoelectoralFrontendApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+
+    // Elecciones generales a nivel Bolivia
+    var eleccionesUrl = '//localhost:3000/api/v1/elecciones?anio={anio}&formato=json';
+
+    $scope.anio = 2009;
+
+    // Funciones
+    var loadElecciones = function() {
+      var url = eleccionesUrl.replace(/{anio}/g, $scope.anio);
+      $http.get(url).then(function(response) {
+        $scope.partidos = response.data.dpas[0].partidos;
+        $scope.eleccion = response.data.eleccion;
+        // Color plomo para los partidos sin color
+        $scope.partidos = $scope.partidos.map(function(p) {
+          if (!p.color) {
+            p.color = '808080';
+          }
+          return p;
+        });
+      }, function(err) {
+        throw err;
+      });
+    };
+
+    loadElecciones();
+
+    // Acciones controlador
+    $scope.setAnio = function(anio) {
+      $scope.anio = anio;
+      loadElecciones();
+    };
+
   });
