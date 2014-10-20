102a17adb0795341e341d1d4041a557d8f2c3688
Marcelo Alejo <malejo@adsib.gob.bo>
2014-10-20 14:19:12 -0400

    Menu  nav anios tipo dropdown para dispositivos moviles.

---
+++ app/scripts/directives/panel.js
@@ -0,0 +1,34 @@
+'use strict';
+/**
+ * @ngdoc directive
+ * @name geoelectoralFrontendApp.directive:resize
+ * @description
+ * # resizePanel
+ */
+angular.module('geoelectoralFrontendApp')
+  .directive('resize', function ($window) {
+        return function (scope, element) {
+        var w = angular.element($window);
+        scope.getWindowDimensions = function () {
+            return {'h': w.height(),'w': w.width()};
+        };
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+            scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+
+            scope.anios_columna = function(){
+                return (newValue.w<768)?"":"col-xs-3";
+            };
+            scope.anios_tipo = function () {
+                return (newValue.w<768)?"dropdown-menu":"nav";
+            };
+            scope.mapa_columna = function(){
+                return (newValue.w<768)?"":"col-xs-9";
+            };
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+});
