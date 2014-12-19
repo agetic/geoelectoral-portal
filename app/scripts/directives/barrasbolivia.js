04007d1b08cb0114b9d59c5c63c5e92f26a7b82b
Marcelo Alejo <malejo@adsib.gob.bo>
2014-12-19 13:30:11 -0400

    Se movio el texto porcentaje fuera de las barras.

--- app/scripts/directives/barrasbolivia.js
+++ app/scripts/directives/barrasbolivia.js
@@ -69,6 +69,24 @@ angular.module('geoelectoralFrontendApp')
           return esTextoMayor(texto, data) ? 'white' : colorBarra;
         };
 
+        function hexToRgb(hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null;
+        }
+        function rgbToHex(r, g, b) {
+            return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+        }
+        function colorInverso(hex){
+          var c = hexToRgb(hex);
+          if(c)
+          { hex = rgbToHex(255-c.r, 255-c.g, 255-c.b); }
+          return hex;
+        }
+
         svg.append('g')
             .attr('class', 'x axis')
             .call(xAxis);
@@ -85,9 +103,11 @@ angular.module('geoelectoralFrontendApp')
             .attr('x', function(d) { return x(d.porcentaje); })
             .attr('text-anchor', 'start')
             .attr('dy', '.35em')
+            //.style('fill',function(d) { if (esTextoMayor(this, d)) return colorInverso('#'+d.color); else return '#000000'; } )
             .attr('dx', function(d) {
               if (esTextoMayor(this, d)) {
-                return -Math.ceil(this.getBBox().width) - marginTexto;
+                //return -Math.ceil(this.getBBox().width) - marginTexto;
+                return marginTexto;
               } else {
                 return marginTexto;
               }
